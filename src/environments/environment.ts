97f0e38e1b0442291a89c52c3ba1c54c4d009d7f
Ayush Gupta <ayushgupta.9350@gmail.com>
2023-09-24 13:26:37 +0530

    Weather App
    code push

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '388813b14dmshd362a4787f19a37p11ec80jsn267ec1e99545',
+
+};
